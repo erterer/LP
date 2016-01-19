e931487b1d900d38937d20e25a9c6e4f290f66e7
Sven Nottelman <sven.nottelman@student.fontys.nl>
2016-01-19 14:44:12 +0100

    Templates documentatie
    Documentatie templates gemaakt. Programma is aangemaakt maar nog niet
    gevuld

---
+++ LifePerformance - Sven Nottelman/LifePerformance - Sven Nottelman/Data/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifePerformance___Sven_Nottelman.Data
+{
+    public class Database
+    {
+
+    }
+}
